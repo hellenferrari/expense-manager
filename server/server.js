4f5a64ff7581a043b89a68141a341bceec815002
Hellen Ferrari <hferrari@compliahealth.com>
2019-03-30 19:53:31 -0700

    deploying to heroku

--- server/server.js
+++ server/server.js
@@ -1,15 +1,29 @@
-const path = require('path');
+ const path = require('path');
+// const express = require('express');
+// const app = express();
+ const publicPath = path.join(__dirname, '..', 'public');
+// const port = process.env.PORT || 3000;
+
+// app.use(express.static(publicPath));
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(publicPath, 'index.html'));
+// });
+
+// app.listen(port, () => {
+//   console.log('Server is up!');
+// });
+
 const express = require('express');
+//const path = require('path');
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
-const port = process.env.PORT || 3000;
 
-app.use(express.static(publicPath));
+app.use(express.static(path.join(publicPath, 'build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(port, () => {
-  console.log('Server is up!');
+app.listen(9000, () => {
+  console.log('server is up!');
 });
