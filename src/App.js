2885a2cb4cddeef0507e047288be4bff8a4f34dc
Hellen Ferrari <hferrari@compliahealth.com>
2019-03-30 19:17:51 -0700

    Added node server for heroku

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import './styles/styles.scss';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense }  from './actions/expenses';
-import getVisibleExpenses from './selectors/expenses';
-import { setTextFilter } from './actions/filters';
 
 const store = configureStore();
 
