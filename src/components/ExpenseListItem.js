3a2b0f974eace14fd8ab1c69b7b6c53b77776f30
Hellen <hellenc.ferrari@gmail.com>
2019-04-03 23:02:53 -0700

    Added filter by Dates

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { removeExpense } from '../actions/expenses';
 import { Link } from 'react-router-dom';
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
