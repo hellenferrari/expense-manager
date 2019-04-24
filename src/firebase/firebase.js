0bdba33d6cd85b597dde35f970afcc8e913a8ca2
Hellen <hellen@Hellens-iMac.local>
2019-04-23 20:49:48 -0700

    Setup test database env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,18 +1,16 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyB-f13ZNFRo5RnbIgnRZBcQTAF9d3FFVlY",
-    authDomain: "expense-manager-6c67b.firebaseapp.com",
-    databaseURL: "https://expense-manager-6c67b.firebaseio.com",
-    projectId: "expense-manager-6c67b",
-    storageBucket: "expense-manager-6c67b.appspot.com",
-    messagingSenderId: "944742516266"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
 };
   
 firebase.initializeApp(config);
 
 const database = firebase.database();
 
-database.ref().set({
-    name: ''    
-});
+export { firebase, database as default };
