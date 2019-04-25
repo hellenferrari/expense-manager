64d386843d17d6e9adb77906f4b668f447894518
Hellen <hellen@Hellens-iMac.local>
2019-04-24 19:26:17 -0700

    WIP

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ import './firebase/firebase';
 
 const store = configureStore();
 
+// if (process.env.NODE_ENV === 'test') {
+//   require('dotenv').config({ path: '.env.test' });
+// } else if (process.env.NODE_ENV === 'development') {
+//   require('dotenv').config({ path: '.env.development' });
+// }
+
 class App extends Component {
   render() {
     return (
